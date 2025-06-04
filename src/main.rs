6ebfb3b3f688da45adc8e3b35482f4ca87b62233
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-06-04 11:48:05 +0100

    added necessary constants

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::time::{Duration, Instant};
 use std::env;
 use std::io::{self, ErrorKind};
 
+//Payload prefix to identify our packet
+const PAYLOAD_PREFIX: &[u8] = b"RUST_UDP_PING"; 
+const DEFAULT_TIMEOUT_SECONDS :u64 = 2; //Default timeout for reply
+const DEFAULT_PAYLOAD: &str = "Default Echo";
+
 fn main() {
     println!("Hello, world!");
 }
