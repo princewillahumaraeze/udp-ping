3d20948654585e02d33b47c1d68a17b2193d6158
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-06-04 12:08:51 +0100

    created the placeholders for functions

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,40 @@ struct PacketConfig{
     source_ip: String,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn print_usage(program_name: &str){
+    unimplemented!()
+}
+
+fn parse_arguments(args: Vec<String>) -> Result<PacketConfig, io::Error>{
+    unimplemented!()
+}
+
+fn create_socket(local_ip_str: &str) -> io::Result<UdpSocket>{
+    unimplemented!()
+}
+
+fn build_socket_payload(sequence_payload: u32,
+     custom_payload_str:&str
+)->Vec<u8>{
+    unimplemented!()
+}
+
+fn perform_echo_ping(socket: &UdpSocket,
+    target_addr:SocketAddr,
+    sequence_number:u32,
+    payload_str:&str
+) -> io::Result<()>{
+    unimplemented!()
+}
+
+fn main() -> io::Result<()>{
+    // Argument parsing
+    let config = match parse_arguments(env::args().collect()){
+        Ok(cfg) => cfg,
+        Err(e) => {
+            print_usage(&env::args().next().unwrap_or_else(|| "udp_ping".to_string()));
+            return Err(e);
+        }
+    };
+
 }
