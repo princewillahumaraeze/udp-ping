223ecf1dccdeb8a044b83e24beaa8054a4128701
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-06-04 11:51:18 +0100

    packet config struct created

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ const PAYLOAD_PREFIX: &[u8] = b"RUST_UDP_PING";
 const DEFAULT_TIMEOUT_SECONDS :u64 = 2; //Default timeout for reply
 const DEFAULT_PAYLOAD: &str = "Default Echo";
 
+struct PacketConfig{
+    target_addr: SocketAddr,
+    payload: String,
+    source_ip: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
